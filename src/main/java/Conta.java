8f3bb9688990baa0a8b16b36b995b7a9c095d603
Felipe Portari <felipe.portari7@gmail.com>
2023-11-30 14:49:45 -0300

    Corrigindo Erros

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -3,52 +3,52 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Conta {
-	
-    private double saldo; // Saldo da conta
-    private RuntimeException exception; // Variável para lidar com exceções
 
-    // Configuração: Cliente especial com saldo inicial negativo
+    private double saldo; // Saldo da conta
+    private RuntimeException exception; // Variï¿½vel para lidar com exceï¿½ï¿½es
+
+    // Configuraï¿½ï¿½o: Cliente especial com saldo inicial negativo
     @Given("^Um cliente especial com saldo atual de -(\\d+) reais$")
     public void um_cliente_especial_com_saldo_atual_de_reais(int sI) {
-    	saldo = -sI;
-    }
-    
-    // Configuração: Cliente comum com um saldo inicial negativo
-    @Given("^Um cliente comum com saldo atual de -(\\d+) reais$")
-    public void um_cliente_comum_com_saldo_atual_de_reais(int sI) {
-    	saldo = -sI;
+        saldo = -sI;
     }
 
-    // Ação: Solicitar um saque
+    // Configuraï¿½ï¿½o: Cliente comum com um saldo inicial negativo
+    @Given("^Um cliente comum com saldo atual de -(\\d+) reais$")
+    public void um_cliente_comum_com_saldo_atual_de_reais(int sI) {
+        saldo = -sI;
+    }
+
+    // Aï¿½ï¿½o: Solicitar um saque
     @When("^for solicitado um saque no valor de (\\d+) reais$")
     public void for_solicitado_um_saque_no_valor_de_reais(int vS) {
         if (vS <= 0) {
-            exception = new RuntimeException("Valor de saque inválido."); // Capturar exceções
+            exception = new RuntimeException("Valor de saque invï¿½lido."); // Capturar exceï¿½ï¿½es
         } else if (saldo < vS) {
-            exception = new RuntimeException("Saldo insuficiente para o saque."); // Capturar exceções
+            exception = new RuntimeException("Saldo insuficiente para o saque."); // Capturar exceï¿½ï¿½es
         } else {
             saldo -= vS; // Realizar o saque
         }
     }
 
-    // Verificação: Saque efetuado com sucesso
+    // Verificaï¿½ï¿½o: Saque efetuado com sucesso
     @Then("^deve efetuar o saque e atualizar o saldo da conta para -(\\d+) reais$")
     public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(int novoSaldo) {
         if (saldo == -novoSaldo) {
-            System.out.println("Saque realizado! Seu novo saldo é: " + saldo);
+            System.out.println("Saque realizado! Seu novo saldo ï¿½: " + saldo);
         } else {
             System.out.println("Ops! Algo deu errado ao tentar realizar o saque.");
         }
     }
 
-    // Verificação: Saque não deve ser efetuado devido a saldo insuficiente
-    @Then("Não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente.")
+    // Verificaï¿½ï¿½o: Saque nï¿½o deve ser efetuado devido a saldo insuficiente
+    @Then("NÃ£o deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente.")
     public void nao_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_Saldo_Insuficiente() {
-        // Verificar se uma exceção foi lançada
+        // Verificar se uma exceÃ§Ã£o foi lanÃ§ada
         if (exception != null) {
-            // Verificar se a mensagem da exceção é "Saldo insuficiente para o saque."
+            // Verificar se a mensagem da exceÃ§Ã£o Ã© "Saldo insuficiente para o saque."
             if ("Saldo insuficiente para o saque.".equals(exception.getMessage())) {
-                System.out.println("Não foi possível realizar o saque. Seu saldo é insuficiente.");
+                System.out.println("NÃ£o foi possÃ­vel realizar o saque. Seu saldo Ã© insuficiente.");
             } else {
                 System.out.println("Ocorreu um erro desconhecido.");
             }
--- src/test/java/teste.feature
+++ src/test/java/teste.feature
@@ -18,8 +18,8 @@
 #Sample Feature Definition Template
 @tag
 Feature: Cliente faz saque de dinheiro como um cliente,
-eu gostaria de sacar dinheiro em caixa eletrÙnico,
-para que eu n„o tenha que esperar em uma fila do banco.
+eu gostaria de sacar dinheiro em caixa eletr√¥nico,
+para que eu n√£o tenha que esperar em uma fila do banco.
 
   @tag1
   Scenario: Cliente especial com saldo negativo
@@ -31,4 +31,4 @@ para que eu n„o tenha que esperar em uma fila do banco.
   Scenario Outline: Cliente comum com saldo negativo
     Given Um cliente comum com saldo atual de -200 reais
     When for solicitado um saque no valor de 200 reais
-    Then N„o deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente.
+    Then N√£o deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente.
